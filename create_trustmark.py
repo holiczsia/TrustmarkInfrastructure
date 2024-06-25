0aa4f39453f82f937862c9cd11d2c10802327c9c
Jiwon Kim <holiczsia@tamu.edu>
2024-06-25 14:24:34 -0500

    commit prerequisites

--- create_trustmark.py
+++ create_trustmark.py
@@ -1,71 +1,82 @@
-#!/usr/bin/env python3
-
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+from datetime import datetime, timedelta, timezone
 
-# Define the namespaces
-namespaces = {
-    'tf': 'https://trustmarkinitiative.org/specifications/trustmark-framework/1.4/schema/',
-    'ds': 'http://www.w3.org/2000/09/xmldsig#',
-    'xsi': 'http://www.w3.org/2001/XMLSchema-instance'
-}
+# Function to create a subelement with text
+def create_subelement(parent, tag, text):
+    element = ET.SubElement(parent, tag)
+    element.text = text
+    return element
 
-# Register namespaces
-for prefix, uri in namespaces.items():
-    ET.register_namespace(prefix, uri)
+# Function to gather input and create XML structure
+def create_trustmark_xml():
+    trustmark = ET.Element("tf:Trustmark", {
+        "tf:id": "trustmark",
+        "xmlns:tf": "https://trustmarkinitiative.org/specifications/trustmark-framework/1.4/schema/",
+        "xmlns:ds": "http://www.w3.org/2000/09/xmldsig#",
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance"
+    })
+    
+    tf_identifier = create_subelement(trustmark, "tf:Identifier", input("Enter Trustmark Identifier: "))
+    
+    tf_definition_ref = ET.SubElement(trustmark, "tf:TrustmarkDefinitionReference")
+    create_subelement(tf_definition_ref, "tf:Identifier", input("Enter Trustmark Definition Reference Identifier: "))
+    
+    # Set issue date to current date and time
+    issue_datetime = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+    create_subelement(trustmark, "tf:IssueDateTime", issue_datetime)
+    
+    # Set expiration date to 90 days after issue date
+    expiration_datetime = (datetime.now(timezone.utc) + timedelta(days=90)).strftime('%Y-%m-%dT%H:%M:%SZ')
+    create_subelement(trustmark, "tf:ExpirationDateTime", expiration_datetime)
+    
+    create_subelement(trustmark, "tf:PolicyURL", input("Enter Policy URL: "))
+    create_subelement(trustmark, "tf:RelyingPartyAgreementURL", input("Enter Relying Party Agreement URL: "))
+    create_subelement(trustmark, "tf:StatusURL", input("Enter Status URL: "))
+    
+    provider = ET.SubElement(trustmark, "tf:Provider")
+    create_subelement(provider, "tf:Identifier", input("Enter Provider Identifier: "))
+    create_subelement(provider, "tf:Name", input("Enter Provider Name: "))
+    
+    provider_contact = ET.SubElement(provider, "tf:Contact")
+    create_subelement(provider_contact, "tf:Kind", "PRIMARY")
+    create_subelement(provider_contact, "tf:Email", input("Enter Provider Contact Email: "))
+    
+    recipient = ET.SubElement(trustmark, "tf:Recipient")
+    create_subelement(recipient, "tf:Identifier", input("Enter Recipient Identifier: "))
+    create_subelement(recipient, "tf:Name", input("Enter Recipient Name: "))
+    
+    recipient_contact = ET.SubElement(recipient, "tf:Contact")
+    create_subelement(recipient_contact, "tf:Kind", "PRIMARY")
+    create_subelement(recipient_contact, "tf:Email", input("Enter Recipient Contact Email: "))
+    
+    return trustmark
 
-def create_element(tag, text=None):
-    """Helper function to create an element with namespaces."""
-    elem = ET.Element(f'{{{namespaces["tf"]["ds"]["xsi"]}}}{tag}')
-    if text:
-        elem.text = text
-    return elem
+# Create XML structure
+trustmark_xml = create_trustmark_xml()
 
-# Create the root element
-trustmark = create_element('Trustmark')
+# Convert to a string with pretty print
+xml_str = ET.tostring(trustmark_xml, encoding="utf-8")
+parsed_str = minidom.parseString(xml_str)
+pretty_str = parsed_str.toprettyxml(indent="    ")
 
-# Add required sub-elements with their content
-provider_id = create_element('ProviderId', 'https://provider.example')
-trustmark.append(provider_id)
+# Customizing the pretty print to match the style of Trustmark header
+pretty_str = pretty_str.replace(' tf:id="trustmark"', '')
+pretty_str = pretty_str.replace(' xmlns:tf=', '\n    xmlns:tf=')
+pretty_str = pretty_str.replace(' xmlns:ds=', '\n    xmlns:ds=')
+pretty_str = pretty_str.replace(' xmlns:xs=', '\n    xmlns:xs=')
+pretty_str = pretty_str.replace('<tf:Trustmark\n    xmlns:tf=', '<tf:Trustmark tf:id="trustmark"\n    xmlns:tf=')
 
-trustmark_def_id = create_element('TrustmarkDefinitionId', 'https://trustmark.example/trustmark-def-id')
-trustmark.append(trustmark_def_id)
 
-trustmark_id = create_element('TrustmarkId', 'https://trustmark.example/trustmark-id')
-trustmark.append(trustmark_id)
+# Prompt for output file name
+output_file_name = input("Enter the output file name (with .xml extension): ")
 
-recipient_id = create_element('RecipientId', 'https://recipient.example')
-trustmark.append(recipient_id)
+# Hard-coded output file path
+output_file_path = f"./trustmark/{output_file_name}"
 
-issue_date_time = create_element('IssueDateTime', '2024-01-01T00:00:00Z')
-trustmark.append(issue_date_time)
 
-expiration_date_time = create_element('ExpirationDateTime', '2025-01-01T00:00:00Z')
-trustmark.append(expiration_date_time)
+# Save to file
+with open(output_file_path, "w", encoding="utf-8") as xml_file:
+    xml_file.write(pretty_str)
 
-status_url = create_element('StatusURL', 'https://provider.example/status')
-trustmark.append(status_url)
-
-relying_party_agreement_url = create_element('RelyingPartyAgreementURL', 'https://provider.example/relying-party-agreement')
-trustmark.append(relying_party_agreement_url)
-
-# Adding a nested subelement example under <tf:ProviderId>
-provider_info = create_element('ProviderInfo')
-provider_name = create_element('ProviderName', 'Example Provider Name')
-provider_info.append(provider_name)
-provider_id.append(provider_info)
-
-# Function to prettify the XML
-def prettify_xml(elem):
-    rough_string = ET.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
-
-# Get the pretty XML string
-pretty_xml_string = prettify_xml(trustmark)
-
-# Write to a file
-with open('trustmark.xml', 'w') as f:
-    f.write(pretty_xml_string)
-
-print("XML file 'trustmark.xml' created successfully.")
+print(f"XML file '{output_file_name}' created successfully in the trustmark folder.")
